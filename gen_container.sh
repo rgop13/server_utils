822a02b7c7a0c32b73cb8dd24dc530c7765589ec
Junyoung Son <44837789+rgop13@users.noreply.github.com>
2024-03-26 14:27:23 +0900

    Update gen_container.sh

--- gen_container.sh
+++ gen_container.sh
@@ -1,17 +1,7 @@
-#!bin/bash
+#!/bin/bash
 
-number_regex='[0-9]+'
-while true
-do
-    echo -n "도커 컨테이너를 생성할 대상 서버를 입력하세요 (반드시 숫자만 입력, e.g., 14): "
-    read TARGET_SERVER
-    if [[ $TARGET_SERVER =~ $number_regex ]]; then
-        break
-    else
-        echo "반드시 숫자만을 입력해야 합니다. 다시 입력하세요."
-    fi
-done
-
+TARGET_SERVER=$(hostname | awk '{ split($1,a,"-"); print a[3]; }')
+echo "현재 위치한 서버는 $TARGET_SERVER번 서버입니다."
 while true
 do
     echo -n "대상 사용자 계정명을 입력하세요 (e.g., sjy): "
@@ -41,6 +31,24 @@ do
     fi
 done
 
+while true
+do
+    echo "생성될 컨테이너 이름을 입력하세요. (현재 설정값: $SET_USER_NAME)"
+    echo -n "  * 입력하지 않으실 경우 기본값으로 생성됩니다! 변경이 필요하신 경우에만 새롭게 입력하세요!: "
+    read SET_CONTAINER_NAME
+
+    if [ -z "$SET_CONTAINER_NAME" ]; then
+        SET_CONTAINER_NAME=$SET_USER_NAME
+    fi
+
+    echo "  * 입력된 컨테이너 이름: $SET_CONTAINER_NAME"
+    echo -n "  * 위 값으로 컨테이너를 생성하시겠습니까? [Yy/Nn]: "
+    read CONFIRM_CONTAINER_NAME
+    if [[ $CONFIRM_CONTAINER_NAME =~ ^[Yy]$ ]]; then
+        break
+    fi
+done
+
 # 쿠다 버전이 몇인지 문자열 검색
 REPO_CUDA_VERSION=$(echo $DOCKER_IMAGE_PATH | awk '{ split($1, a, ":"); split(a[2], b, "-"); print b[1]; }')
 
@@ -62,9 +70,9 @@ if [ ! -f "$DOCKER_COMPOSE_FILE" ]; then
     echo "services:" >> "$DOCKER_COMPOSE_FILE"
 fi
 
-# SET_USER_NAME 서비스가 이미 있는지 검사
-if grep -q " $SET_USER_NAME:" "$DOCKER_COMPOSE_FILE"; then
-    echo "Error: Service $SET_USER_NAME already exists in $DOCKER_COMPOSE_FILE"
+# SET_CONTAINER_NAME 서비스가 이미 있는지 검사
+if grep -q " $SET_CONTAINER_NAME:" "$DOCKER_COMPOSE_FILE"; then
+    echo "Error: Container $SET_CONTAINER_NAME already exists in $DOCKER_COMPOSE_FILE"
     exit 1
 else
     # DOCKER IMAGE PATH로 새로운 dockerfile을 임시로 만듦
@@ -77,17 +85,17 @@ else
     fi
 
     # default_container_settings.yaml에서 SET_USER_NAME으로 대체하고 추가할 서비스 내용 준비
-    SERVICE_CONTENT=$(sed "s@\${SET_USER_NAME}@$SET_USER_NAME@g" "$DEFAULT_SETTINGS_FILE" | sed "s/dockerfile: dockerfile/dockerfile: $NEW_DOCKERFILE_NAME/g" | sed "s@image: .*@image: nlpai/cuda:$REPO_CUDA_VERSION@g")
+    SERVICE_CONTENT=$(sed "s@\${SET_USER_NAME}@$SET_USER_NAME@g" "$DEFAULT_SETTINGS_FILE" | sed "s/dockerfile: dockerfile/dockerfile: $NEW_DOCKERFILE_NAME/g" | sed "s@image: .*@image: nlpai/cuda:$REPO_CUDA_VERSION@g" | sed "s@container_name: SET_CONTAINER_NAME/container_name: $SET_CONTAINER_NAME@g")
 
     # TARGET_SERVER의 docker-compose.yaml 파일에 서비스 추가
     echo "$SERVICE_CONTENT" >> "$DOCKER_COMPOSE_FILE"
-    result=$(grep " $SET_USER_NAME:" $DOCKER_COMPOSE_FILE)
+    result=$(grep "$SET_USER_NAME" $DOCKER_COMPOSE_FILE)
     if [[ $result =~ $SET_USER_NAME ]]; then
         echo "Service $SET_USER_NAME has been added to $DOCKER_COMPOSE_FILE"
 
         # 이제 추가된 서비스를 컨테이너로 만들자.
-        docker-compose -f $DOCKER_COMPOSE_FILE up -d $SET_USER_NAME
-        docker-compose -f $DOCKER_COMPOSE_FILE ps
+        sudo docker-compose -f $DOCKER_COMPOSE_FILE up -d $SET_CONTAINER_NAME
+        sudo docker-compose -f $DOCKER_COMPOSE_FILE ps
     else
         echo "뭔가 뭔가 문제 발생... 아마 sed와 같은 문자열 관련 이슈일 것 같음..! 관리자에게 문의 바람..!"
         exit 1
