bfb1d98ea8238da91337b907a5b3e7a26b429234
rgop13 <thop8@naver.com>
2024-05-24 16:46:23 +0900

    added

--- add_unique_user.sh
+++ add_unique_user.sh
@@ -8,10 +8,16 @@ if [[ "$CONFIRM_EXIST" =~ $SET_USER_NAME ]]; then
   # 이 경우, unique_id를 추출해서 해당 unique_id로 계정을 생성한다.
   UNIQUE_UID=$(echo $CONFIRM_EXIST | awk -F ':' '{print $2}')
   sudo useradd $SET_USER_NAME -c $SET_USER_NAME -s /bin/bash -m -d /mnt/raid6/$SET_USER_NAME -u $UNIQUE_UID -g users
+  sudo usermod -aG docker $SET_USER_NAME
+  echo -e "1234\n1234\n" | sudo passwd $SET_USER_NAME
+  sudo passwd -e $SET_USER_NAME
 else
   # 존재하지 않는 계정인 경우, unique_id를 할당하고, 기록함
-  UNIQUE_UID=$(tail $ALL_USERS_FILE -n 1 | awk -F ':' '{print $2+1}')
+  UNIQUE_UID=$(tail /data/sjy/passwd/all_user_ids -n 1 | awk -F ':' '{print $2+1}')
   sudo useradd $SET_USER_NAME -c $SET_USER_NAME -s /bin/bash -m -d /mnt/raid6/$SET_USER_NAME -u $UNIQUE_UID -g users
+  sudo usermod -aG docker $SET_USER_NAME
+  echo -e "1234\n1234\n" | sudo passwd $SET_USER_NAME
+  sudo passwd -e $SET_USER_NAME
   # unique ids 파일 업데이트
   echo "${SET_USER_NAME}:${UNIQUE_UID}" | tee -a /data/sjy/passwd/all_user_ids
 fi
